073dc590337352b88af7498eb749e34cc5f65b8a
Armaan Cheema <71794534+armaancheema484@users.noreply.github.com>
2025-02-03 11:08:15 -0600

    Update script.js

--- javascript_algo_datastructures/pyramid_generator/script.js
+++ javascript_algo_datastructures/pyramid_generator/script.js
@@ -1,12 +1,14 @@
-const character = "#";
-const count = 8;
+const character = "#"; // controls char used in pyramid
+const count = 8; // controls number of rows in pyramid
 const rows = [];
-let inverted = true;
+let inverted = true; // controls inversion of pyramid
 
+// controls spacing for pyramid
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
+// controls creation of pyramid
 for (let i = 1; i <= count; i++) {
   if (inverted) {
     rows.unshift(padRow(i, count));
